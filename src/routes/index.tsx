1c12ab166bdcbeef8378b674dd2f3913659887f4
guilherme augusto <guilherme.silva@ftdata.com>
2022-04-23 19:43:03 -0300

    migrate: migrando de js para typescript

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+
+const Router = () => (
+  <Routes>
+    <Route path="/" element={<Dashboard />} />
+  </Routes>
+);
+
+export default Router;
